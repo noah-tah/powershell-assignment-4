8369a595e4940bdabc8f5372b0d028b5c376913d
noah-tah <noahtah21@gmail.com>
2025-04-24 21:21:40 -0500

    batman

---
+++ powershell-assignment-4-noah-tah.ps1
@@ -0,0 +1,18 @@
+<#
+Noah Tah
+4/24/2025
+Powershell Assignment 4
+Programming II
+Evan Vaverka
+Northwestern Oklahoma State University
+Assignment Description:
+
+Create a program that generates a number between 1 and 10, a number between 1 and 50, and a number between 1 and 100.
+Give the user the option of choosing one of the random three numbers to guess.
+Let the user guess until they get the right answer.
+Tell them if they are too high or too low.
+Also make sure to tell them if they enter something that is not a number.
+#>
+
+
+# Function to generate a random number between 1 and 10
